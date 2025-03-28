8daac7b5a8d8275b8f76584bb37002110c59bebd
FernandoJauregui24110238 <a24110238@live.ceti.mx>
2025-03-28 03:29:57 +0000

    practica 2

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include "Serie.hpp"
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <iostream>
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
 
-Serie serienavidena;
+auto pantalla = Screen::Create(
+     Dimension::Full(),
+     Dimension::Full()
+);
 
-serienavidena.ApagarTodo();
-serienavidena.Imprimir();
-serienavidena.EncenderTodo();
-serienavidena.Imprimir();
-serienavidena.ApagarCantidad(8);
-serienavidena.Imprimir();
-serienavidena.EncenderCantidad(3);
-serienavidena.Imprimir();
-serienavidena.AlternarEncendido();
-serienavidena.Imprimir();
-serienavidena.RecorrerDerecha();
-serienavidena.Imprimir();
-serienavidena.RecorrerIzquierda();
-serienavidena.Imprimir();
+auto documento = vbox(
+  spinner(21,1)
+);
+
+Render(pantalla, documento);
+pantalla.Print();
+
+// Serie serienavidena;
+
+// serienavidena.ApagarTodo();
+// serienavidena.Imprimir();
+// serienavidena.EncenderTodo();
+// serienavidena.Imprimir();
+// serienavidena.ApagarCantidad(8);
+// serienavidena.Imprimir();
+// serienavidena.EncenderCantidad(3);
+// serienavidena.Imprimir();
+// serienavidena.AlternarEncendido();
+// serienavidena.Imprimir();
+// serienavidena.RecorrerDerecha();
+// serienavidena.Imprimir();
+// serienavidena.RecorrerIzquierda();
+// serienavidena.Imprimir();
 
   return 0;
 }
